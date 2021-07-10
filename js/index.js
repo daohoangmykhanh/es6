33d7e00886ac125cbc2771c00f1e4f98b46d66fb
DaoHoangMyKhanh <daohoangmykhanh@gmail.com>
2021-07-10 19:21:41 +0700

    Sua_thong_tin

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ document.querySelector(' .signup-form .actions ul li:last-child a').onclick = fu
     for (let i =0; i <arrInput.length; i++){
         let input = arrInput[i];
         let {id,value} = input;
-        content += id + ' : ' + value + '<br />' ;
+        content += '<br />' + id + ' : ' + value + '<br />';
     }
 
     document.querySelector('.donate-us').innerHTML =  content;
